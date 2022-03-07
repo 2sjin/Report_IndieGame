c1ff0fb69d33a288fe8682873290674bf52f6f8a
2sjin <options3224@gmail.com>
2022-03-07 22:20:42 +0900

    CatEscape 좌우 버튼 UI 추가

--- CatEscape/Assets/PlayerController.cs
+++ CatEscape/Assets/PlayerController.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    void Start() { }
-
     void Update() {
-        // Left 키를 눌렀을 때 왼쪽으로 이동
+        // 키보드의 [Left] 키를 눌렀을 때 왼쪽으로 이동
         if (Input.GetKey(KeyCode.LeftArrow))
             transform.Translate(-0.03f, 0, 0);
 
-        // Right 키를 눌렀을 때 오른쪽으로 이동
+        // 키보드의 [Right] 키를 눌렀을 때 오른쪽으로 이동
         if (Input.GetKey(KeyCode.RightArrow))
             transform.Translate(0.03f, 0, 0);
     }
 
+    // 버튼 LButton을 눌렀을 때 왼쪽으로 이동
     public void LButtonDown() {
         transform.Translate(-1.5f, 0, 0);
     }
 
+    // 버튼 RButton을 눌렀을 때 오른쪽으로 이동
     public void RButtonDown() {
         transform.Translate(1.5f, 0, 0);
     }
