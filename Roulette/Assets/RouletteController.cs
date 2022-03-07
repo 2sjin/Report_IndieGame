2dab258180bd0f3e1c8491e5d229de9da2f0da11
2sjin <options3224@gmail.com>
2022-03-07 18:47:04 +0900

    Roulette 프로젝트 파일 추가

---
+++ Roulette/Assets/RouletteController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour {
+    float rotSpeed = 0;     // 룰렛의 회전 속도
+
+    void Start() { }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0))    // 마우스를 눌렀을 때
+            this.rotSpeed = 5;              // 회전 속도 증가
+        transform.Rotate(0, 0, this.rotSpeed);  // 회전 속도만큼 룰렛 회전
+        rotSpeed *= 0.995f;    // 회전 속도 서서히 감소
+    }
+
+}
