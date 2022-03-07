b8ca274eea7cc939a97893b2871800d0ab7c7ce5
2sjin <options3224@gmail.com>
2022-03-07 19:39:13 +0900

    SwipeCar 오디오 및 거리 UI 추가

--- SwipeCar/Assets/CarController.cs
+++ SwipeCar/Assets/CarController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,11 +14,11 @@ public class CarController : MonoBehaviour {
 
         else if (Input.GetMouseButtonUp(0)) {   // 마우스 버튼을 뗄 때
             endPos = Input.mousePosition;   // 현재 마우스 좌표를 endPos 벡터에 저장
-            float swipeLength = endPos.x - startPos.x;  // 
 
-            this.speed = swipeLength / 5000.0f;
+            float swipeLength = endPos.x - startPos.x;  // 마우스로 스와이프한 길이 계산
+            this.speed = swipeLength / 5000.0f; // 스와이프 길이 보정
 
-            GetComponent<AudioSource>().Play();
+            GetComponent<AudioSource>().Play(); // 오디오 소스 재생
         }
 
         transform.Translate(this.speed, 0, 0);  // X 방향으로 자동차 이동
---
+++ SwipeCar/Assets/GameDirector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+
+    void Start() {
+        // 게임 오브젝트 불러오기
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("distance");
+    }
+
+    void Update() {
+        // 자동차에서 깃발로 가기 위한 거리 계산
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        
+        // 텍스트 컴포넌트에 거리 출력
+        this.distance.GetComponent<Text>().text = "목표 지점까지 " + length.ToString("F2") + "m";
+    }
+
+}
